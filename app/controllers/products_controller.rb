8a476b3cbda2e16c21ac84b4968683c9192a45fd
matthew kim <matthewjkim@gmail.com>
2013-03-23 21:26:58 +0000

    added destroy/update/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,32 @@ class ProductsController < ApplicationController
     end
 	end	
   
+  def show
+    @product = Product.where(id: params[:id]).first
+  end
+  
+  def edit
+     @product = Product.where(id: params[:id]).first
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def update
+    @product = Product.where(:id => params[:id]).first
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, :notice => 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end  
+  
   def create
     @product = Product.new(params[:product])
     
@@ -20,6 +46,16 @@ class ProductsController < ApplicationController
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
+  end
+  
+  def destroy
+    @product = Product.where(:id => params[:id]).first
+    @product.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+  	end		
   end
 
 end
